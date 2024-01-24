efe9b85f8e7ab03e2982af033e2d1ad543546b80
RaghunandanVenkatesh <rnv001@yahoo.co.in>
2024-01-24 09:38:21 +0530

    new data set added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from utils.configreader import Config
-
+from utils.logger import logger
 
 config = Config("configs/config_1.json")
 
+for tts, ttsobj in config.ttsDict.iteritems():
+    logger.info(f"Creating the sound from {tts}")
+    
 print("complted")
  
